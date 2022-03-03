2f67282e0ef0eff60b4330f8ba8a779977db5490
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2022-03-03 12:10:08 +0300

    Assignment3 Added

--- Java Camp/Assignment3/src/Assignment3/Business/Concrete/CampaignManager.java
+++ Java Camp/Assignment3/src/Assignment3/Business/Concrete/CampaignManager.java
@@ -2,4 +2,10 @@ package Assignment3.Business.Concrete;
 import Assignment3.Business.Abstract.*;
 public class CampaignManager implements CampaignService{
 
+	@Override
+	public double calculateNewPrice(double discount) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
